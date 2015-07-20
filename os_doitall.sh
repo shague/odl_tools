66715c1146d6db52e718db482bda8a11d52dedd1
Sam Hague <shague86@gmail.com>
2015-07-19 21:45:13 -0400

    add key

---
+++ os_doitall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source openrc admin admin
+
+/opt/tools/os_addnano.sh
+sleep 1
+/opt/tools/os_addadminkey.sh
+sleep 1
+/opt/tools/os_addextnetrtr.sh
+sleep 3
+/opt/tools/os_addvms.sh
+sleep 5
+/opt/tools/os_addfloatingips.sh
+sleep 1
+/opt/tools/os_addsecuritygroups.sh
