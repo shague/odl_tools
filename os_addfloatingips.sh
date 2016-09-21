ebbdc66cfdf02de76e1f5c77721c049246c68db3
Sam Hague <shague86@gmail.com>
2016-09-21 17:55:56 -0400

    updates for additional vms

--- os_addfloatingips.sh
+++ os_addfloatingips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for vm in vmvx1 vmvx2; do
+for vm in vmvx01 vmvx02 vmvx11 vmvx12; do
     vm_id=$(nova list | grep $vm | awk '{print $2}')
     port_id=$(neutron port-list -c id -c fixed_ips -- --device_id $vm_id | grep subnet_id | awk '{print $2}')
     neutron floatingip-create --port_id $port_id ext-net
--- os_ssh.sh
+++ os_ssh.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-
+set -x
 vmip=$1
-ns=qdhcp-$(neutron net-list | grep vx-net | awk '{print $2}')
+
+if [[ $vmip == 10.100.5.* ]]; then
+    vxnet="vx-net0"
+elif [[ $vmip == 10.100.6.* ]]; then
+    vxnet="vx-net1"
+else
+    echo "Error: Network $vmip not known to $0"
+    exit
+fi
+
+ns=qdhcp-$(neutron net-list | grep $vxnet | awk '{print $2}')
 #ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/odl/.ssh/id_rsa cirros@$1
 #sudo ip netns exec qdhcp-1082db45-5c34-47d5-9c1e-de9314aa1b01 ping 10.100.5.4
 
